076d18dd7990aacb6706b8c9407cb386507d3e9e
Harald Lønsethagen <haraldlons@gmail.com>
2018-03-20 12:41:37 +0100

    add script which makes a new latex project in new folder from current directory.
    Should make it easy to fastly make new latex project.

--- scripts/new_latex_project.sh
+++ scripts/new_latex_project.sh
@@ -3,10 +3,11 @@
 read -p "New LaTeX project name: " project_name
 DATE=`date --date="0 day" +%Y-%m-%d`
 underscore="_"
-foldername=$DATE$underscore$project_name
+_latex="_latex"
+foldername=$DATE$underscore$project_name$_latex
 mkdir $foldername
-cp -R ~/useful_computer_notes/templates/latex_templates/simple_article_latex_template/ .
 cd $foldername
+cp -R ~/useful_computer_notes/templates/latex_templates/simple_article_latex_template/simple_article_latex_template.tex .
 mv simple_article_latex_template.tex $project_name.tex
 subl .
 echo
