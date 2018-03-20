ca097f9bd773346866242e6e1d916f72d293b1b2
Harald Lønsethagen <haraldlons@gmail.com>
2018-03-20 12:40:26 +0100

    add new folder 'installs' for bash-scripts which install useful programs. E.g. install latex compilation packages

---
+++ installs/install_latex_tools_for_sublime.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Goal: Being able to compile .tex files in sublime with ctrl-b for super easy development
+
+# 1: Install Package Control for Sublime Text
+# 2: Install LaTeXTools with Package Control in Sublime Text
+# 3: Run this install script
+
+sudo apt-get install latexmk
+sudo apt-get install texlive-latex-base
+sudo apt-get install texlive-latex-recommended
+sudo apt-get install texlive-latex-extra
+sudo apt-get install texlive-lang-european
