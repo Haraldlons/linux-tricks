a2cd3945bd84b3e8f6a088409729304a2ed6cea4
Harald Lønsethagen <haraldlons@gmail.com>
2018-06-08 12:44:53 +0200

    add install vim with useful programs

--- installs/install_useful_programs.sh
+++ installs/install_useful_programs.sh
@@ -7,3 +7,4 @@ sudo apt-get install sshfs  # Being able to 'link' folder from ssh to your local
 							# You can then e.g. open sublime on local machine and when you
 							# save it will be updated on server
 sudo apt-get update && sudo apt-get install compiz-plugins
+sudo apt-get install vim
