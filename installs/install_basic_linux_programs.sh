cc59a20ab116d7e80fd652ed4f4da8e2eb024642
Harald Lønsethagen <haraldlons@gmail.com>
2018-11-21 21:42:15 +0100

    add install ranger in basic linux programs

--- installs/install_basic_linux_programs.sh
+++ installs/install_basic_linux_programs.sh
@@ -4,10 +4,11 @@
 # so don't worry about disk space
 
 sudo apt-get update
-sudo apt-get --assume-yes install git
+sudo apt-get --assume-yes install git # Version control
 sudo apt-get --assume-yes install sshfs # 'link' folder from ssh
-sudo apt-get --assume-yes install tmux
-sudo apt-get --assume-yes install tilda
-sudo apt-get --assume-yes install vim 
-sudo apt-get --assume-yes install compiz-plugins
-sudo apt-get --assume-yes install tig
+sudo apt-get --assume-yes install tmux # Terminal multiplexer
+sudo apt-get --assume-yes install tilda # Terminal overlay
+sudo apt-get --assume-yes install vim # Editor
+sudo apt-get --assume-yes install compiz-plugins # Ubuntu settings extention
+sudo apt-get --assume-yes install tig # Pretty view of git log + branches
+sudo apt-get --assume-yes install ranger # View filesystem in terminal
